962a7a23322bcc91270b66fecf0cf23cf57d6af4
jorgeta1996 <jorge.mota_13@hotmail.com>
2019-11-14 19:27:33 +0000

    RecyclerAdapter, item_layout, strings

--- app/src/main/java/pt/ipleiria/projetopdm/AddActivity.java
+++ app/src/main/java/pt/ipleiria/projetopdm/AddActivity.java
@@ -1,14 +1,36 @@
 package pt.ipleiria.projetopdm;
 
+
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import in.galaxyofandroid.spinerdialog.OnSpinerItemClick;
+import in.galaxyofandroid.spinerdialog.SpinnerDialog;
 
 public class AddActivity extends AppCompatActivity {
+
+    private SpinnerDialog spinnerDialog;
+    private ArrayList<String> items;
+    private TextView textViewSpinnerDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,12 +54,31 @@ public class AddActivity extends AppCompatActivity {
             }
         });
 
-        ArrayAdapter<String> adapterSpinnerVehicle = new ArrayAdapter<String>(AddActivity.this, android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.classes));
+        ArrayAdapter<String> adapterSpinnerVehicle = new ArrayAdapter<>(AddActivity.this, android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.classes));
         adapterSpinnerVehicle.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerVehicle.setAdapter(adapterSpinnerVehicle);
 
-        ArrayAdapter<String> adapterSpinnerCountries = new ArrayAdapter<String>(AddActivity.this, android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.countries));
-        adapterSpinnerCountries.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinnerCountries.setAdapter(adapterSpinnerCountries);
+//        ArrayAdapter<String> adapterSpinnerCountries = new ArrayAdapter<>(AddActivity.this, android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.countries));
+//        adapterSpinnerCountries.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        spinnerCountries.setAdapter(adapterSpinnerCountries);
+
+
+
+        textViewSpinnerDialog = findViewById(R.id.textViewTesteSpinner);
+        items = new ArrayList<>(Arrays.asList(getResources().getStringArray(R.array.listaMotas)));
+        spinnerDialog = new SpinnerDialog(AddActivity.this, items, "Select items");
+        spinnerDialog.bindOnSpinerListener(new OnSpinerItemClick() {
+            @Override
+            public void onClick(String item, int position) {
+                textViewSpinnerDialog.setText(item);
+            }
+        });
+
+        textViewSpinnerDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                spinnerDialog.showSpinerDialog();
+            }
+        });
     }
 }
