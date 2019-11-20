d6e15c752bbb7ee3efbf625c9ccbb13861157857
jorgeta1996 <jorge.mota_13@hotmail.com>
2019-11-20 15:37:22 +0000

    Alterar foto atraves dos botões

--- app/src/main/java/pt/ipleiria/projetopdm/AddActivity.java
+++ app/src/main/java/pt/ipleiria/projetopdm/AddActivity.java
@@ -2,7 +2,12 @@ package pt.ipleiria.projetopdm;
 
 
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -10,6 +15,8 @@ import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -22,12 +29,18 @@ import in.galaxyofandroid.spinerdialog.SpinnerDialog;
 
 public class AddActivity extends AppCompatActivity {
 
+    public static final String NEW_VEHICLE = "NEWVEHICLE";
+    public static final int GALLERY_REQUEST_CODE_ADD = 1;
+    public static final int REQUEST_TAKE_PHOTO = 2;
+    private boolean read;
+
     private SpinnerDialog spinnerDialog;
     private ArrayList<String> items;
     private TextView textViewSpinnerDialog;
     private TextView textViewSpinnerCountriesDialog;
     private ImageView imageVehicle;
     private int flagPicture;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -122,11 +135,51 @@ public class AddActivity extends AppCompatActivity {
     }
 
     public void onClickButtonPicture(View view) {
-        flagPicture=1;
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (cameraIntent.resolveActivity(this.getPackageManager()) != null) {
+            startActivityForResult(cameraIntent, REQUEST_TAKE_PHOTO);
+        }
     }
 
     public void onClickButtonGallery(View view) {
-        flagPicture=1;
+        Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(Intent.createChooser(intent, getString(R.string.txtSelectPhoto)), GALLERY_REQUEST_CODE_ADD);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK)
+            switch (requestCode) {
+                case GALLERY_REQUEST_CODE_ADD:
+                    try {
+                        Uri selectedImageUri = data.getData();
+                        Glide.with(this)
+                                .asBitmap()
+                                .load(selectedImageUri)
+                                .override(300, 300)
+                                .into(imageVehicle);
+                        read = true;
+                        flagPicture=1;
+                    } catch (Exception e) {
+                        Log.e("GestorVeiculos", getString(R.string.txtErrorFile), e);
+                        read = false;
+                    }
+                    break;
+                case REQUEST_TAKE_PHOTO:
+                    try {
+                        if (data != null && data.getExtras() != null) {
+                            Bitmap imageBitmap = (Bitmap) data.getExtras().get("data");
+                            imageVehicle.setImageBitmap(imageBitmap);
+                            read = true;
+                            flagPicture=1;
+                        }
+                    } catch (Exception e) {
+                        Log.e("GestorVeiculos", getString(R.string.txtErrorFile), e);
+                        read = false;
+                    }
+            }
     }
 
     public void onClickButtonAdd(View view) {
