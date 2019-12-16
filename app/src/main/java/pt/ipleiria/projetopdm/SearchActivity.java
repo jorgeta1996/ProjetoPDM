3163b0d2bb5292a8f911629f476bddb2d4c0dad2
Aléxio <alexiocardoso@hotmail.com>
2019-12-16 12:30:50 +0000

    seRCH

--- app/src/main/java/pt/ipleiria/projetopdm/SearchActivity.java
+++ app/src/main/java/pt/ipleiria/projetopdm/SearchActivity.java
@@ -1,21 +1,20 @@
 package pt.ipleiria.projetopdm;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.net.Uri;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.Toast;
 
-import java.util.ArrayList;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
 
 import pt.ipleiria.projetopdm.modelo.GestorVeiculos;
 
@@ -24,15 +23,47 @@ public class SearchActivity extends AppCompatActivity implements AdapterView.OnI
     private ListView listViewV;
     private GestorVeiculos gestorVeiculos;
 
+
+    /**
+     * Variáveis para Toolbar
+     */
+    private DrawerLayout mDrawer;
+    private Toolbar toolbar;
+    private NavigationView nvDrawer;
+    private ActionBarDrawerToggle drawerToggle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
 
-        Intent i = getIntent();
-        listViewV =  findViewById(R.id.listview_searchActivity);
-        listViewV.setOnItemClickListener(this);
+//        Intent i = getIntent();
+//        listViewV =  findViewById(R.id.listview_searchActivity);
+//        listViewV.setOnItemClickListener(this);
+
+
+
+
+
+
+
+
+
+        /** Toolbar **/
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        mDrawer = findViewById(R.id.drawer_layout);
+        nvDrawer = findViewById(R.id.nvView);
+        setupDrawerContent(nvDrawer);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        mDrawer = findViewById(R.id.drawer_layout);
+        drawerToggle = setupDrawerToggle();
+        drawerToggle.syncState();
+        mDrawer.addDrawerListener(drawerToggle);
 
     }
 
@@ -58,6 +89,84 @@ public class SearchActivity extends AppCompatActivity implements AdapterView.OnI
     public void onPointerCaptureChanged(boolean hasCapture) {
 
     }
+
+
+    /**
+     * --------------------------------------Métodos para a Navigation Drawer-------------------------------
+     **/
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        selectDrawerItem(menuItem);
+                        return true;
+                    }
+                });
+    }
+
+
+    public void selectDrawerItem(MenuItem menuItem) {
+
+        switch (menuItem.getItemId()) {
+            case R.id.nav_home:
+                Intent i1 = new Intent(this, MainActivity.class);
+                startActivity(i1);
+                break;
+            case R.id.nav_search:
+//                Intent i2 = new Intent(this, MainActivity.class);
+//                startActivity(i2);
+                break;
+            case R.id.nav_add:
+                Intent i3 = new Intent(this, AddActivity.class);
+                startActivity(i3);
+                break;
+            case R.id.nav_share:
+//                Intent i4 = new Intent(this, MainActivity.class);
+//                startActivity(i4);
+                break;
+            case R.id.nav_feedback:
+//                Intent i5 = new Intent(this, MainActivity.class);
+//                startActivity(i5);
+                Intent intent = Intent.makeMainSelectorActivity(Intent.ACTION_MAIN, Intent.CATEGORY_APP_EMAIL);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);//Min SDK 15
+                startActivity(intent);
+                break;
+            case R.id.nav_info:
+//                Intent i6 = new Intent(this, MainActivity.class);
+//                startActivity(i6);
+                break;
+            case R.id.nav_leave:
+                finish();
+
+
+                break;
+
+            default:
+
+        }
+
+        menuItem.setChecked(true);
+        setTitle(menuItem.getTitle());
+        mDrawer.closeDrawers();
+    }
+
+    private ActionBarDrawerToggle setupDrawerToggle() {
+        return new ActionBarDrawerToggle(this, mDrawer, toolbar, R.string.drawer_open, R.string.drawer_close);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+    /**-------------------------------------- /Métodos para a Navigation Drawer-------------------------------**/
 
 
 }
