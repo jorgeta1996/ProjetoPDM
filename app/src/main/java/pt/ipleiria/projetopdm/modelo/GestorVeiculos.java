012d6d1b18eabbd37288865c14595952c9767160
jorgeta1996 <jorge.mota_13@hotmail.com>
2020-01-21 17:48:34 +0000

    as

--- app/src/main/java/pt/ipleiria/projetopdm/modelo/GestorVeiculos.java
+++ app/src/main/java/pt/ipleiria/projetopdm/modelo/GestorVeiculos.java
@@ -22,6 +22,10 @@ public class GestorVeiculos implements Serializable {
     private GestorVeiculos() {
     }
 
+    /**
+     * Método para adicionar veiculo à lista de veiculos
+     * @param veiculo Veiculo a adicionar
+     */
     public void adicionarVeiculo(Veiculo veiculo){
         if (!veiculos.contains(veiculo)){
             veiculos.add(veiculo);
@@ -29,6 +33,11 @@ public class GestorVeiculos implements Serializable {
         }
     }
 
+    /**
+     * Método para atualizar o veiculo da lista de veiculos
+     * @param pos Posição do veiculo na lista
+     * @param veiculo Veiculo a atualizar
+     */
     public void atualizarVeiculo(int pos, Veiculo veiculo) {
         if (!veiculos.contains(veiculo) || veiculo.getMatricula().equalsIgnoreCase(veiculos.get(pos).getMatricula())) {
             veiculos.set(pos, veiculo);
@@ -36,23 +45,44 @@ public class GestorVeiculos implements Serializable {
         }
     }
 
-
+    /**
+     * Método para obter veiculo através da posição
+     * @param pos Posição do veiculo na lista
+     * @return Veiculo na posição
+     */
     public Veiculo obterVeiculo(int pos){
         return veiculos.get(pos);
     }
 
+    /**
+     * Método para receber lista de veiculos
+     * @return lista de veiculos
+     */
     public ArrayList<Veiculo> getVeiculos(){
         return veiculos;
     }
 
+    /**
+     * Método para remover veiculo na posição dada
+     * @param pos Posição do veiculo na lista
+     */
     public void removerVeiculo(int pos){
         veiculos.remove(pos);
     }
 
+    /**
+     * Método para obter posição do veiculo na lista
+     * @param veiculo Veiculo a procurar
+     * @return Posição do veiculo na lista
+     */
     public int obterPosVeiculo(Veiculo veiculo){
         return veiculos.indexOf(veiculo);
     }
 
+    /**
+     * Método toString
+     * @return String com todos os toStrings dos veiculos existentes
+     */
     @NonNull
     @Override
     public String toString() {
